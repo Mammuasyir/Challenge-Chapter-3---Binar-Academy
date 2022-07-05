a3597b31922625e4d8ee1d64b5a1429e4850faba
triman <trimanrahmadi@gmail.com>
2022-07-05 17:12:26 +0700

    ok

--- app/src/main/java/com/humam/challengechapter3/GJepang/utils/Jepang.kt
+++ app/src/main/java/com/humam/challengechapter3/GJepang/utils/Jepang.kt
@@ -1,20 +1,20 @@
 package com.humam.challengechapter3.hopimpa.utils
 
-//fun main() {
-//    println("===========================================================================")
-//    println("GAMETERMINAL VERSION")
-//    println("===========================================================================")
-//    println("Pilihan 2")
-//    println("Mulai Bermain:")
-//    println("Masukkan pemain 1:")
-//    var input1 = readLine().toString()
-//    println("Masukkan Pemain 2")
-//    var input2 = readLine().toString()
-//    val pemain1 = Player(input1)
-//    val pemain2 = Player(input2)
-//    val hasilGame = compare(pemain1, pemain2)
-//    println("Hasil: \n$hasilGame")
-//}
+fun main() {
+    println("===========================================================================")
+    println("GAMETERMINAL VERSION")
+    println("===========================================================================")
+    println("Pilihan 2")
+    println("Mulai Bermain:")
+    println("Masukkan pemain 1:")
+    var input1 = readLine().toString()
+    println("Masukkan Pemain 2")
+    var input2 = readLine().toString()
+    val pemain1 = Player(input1)
+    val pemain2 = Player(input2)
+    val hasilGame = compare(pemain1, pemain2)
+    println("Hasil: \n$hasilGame")
+}
 
 class RumusJepang{
 
